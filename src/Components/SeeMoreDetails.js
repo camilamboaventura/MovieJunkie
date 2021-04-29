4cb88f521d689dff4d03e073311f2760f84c564d
Camila Boaventura <camilamboaventura@gmail.com>
2021-04-30 00:07:52 +0200

    cast added to seeMoreDetails component

--- src/Components/SeeMoreDetails.js
+++ src/Components/SeeMoreDetails.js
@@ -4,21 +4,35 @@ import axios from "axios";
 class SeeMoreDetails extends Component {
   state = {
     mediaDetails: {},
+    cast: [],
   };
 
   componentDidMount = async () => {
-    console.log(this.props.match.params);
     try {
       if (this.props.match.params.location === "searchMovies") {
         const moviesResponse = await axios.get(
           `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=125c6992a7675d6c3e35696ea71a8c59`
         );
-        this.setState({ mediaDetails: { ...moviesResponse.data } });
+        const moviesCreditsResponse = await axios.get(
+          `https://api.themoviedb.org/3/movie/${this.props.match.params.id}/credits?api_key=125c6992a7675d6c3e35696ea71a8c59`
+        );
+        this.setState({
+          mediaDetails: { ...moviesResponse.data },
+          cast: [...moviesCreditsResponse.data.cast],
+        });
       } else {
         const seriesResponse = await axios.get(
           `https://api.themoviedb.org/3/tv/${this.props.match.params.id}?api_key=125c6992a7675d6c3e35696ea71a8c59`
         );
-        this.setState({ mediaDetails: { ...seriesResponse.data } });
+        const seriesCreditResponse = await axios.get(
+          `https://api.themoviedb.org/3/tv/${this.props.match.params.id}/credits?api_key=125c6992a7675d6c3e35696ea71a8c59`
+        );
+        
+        console.log(seriesCreditResponse.data.cast);
+        this.setState({
+          mediaDetails: { ...seriesResponse.data },
+          cast: [...seriesCreditResponse.data.cast]
+        });
       }
     } catch (err) {
       console.error(err);
@@ -26,78 +40,101 @@ class SeeMoreDetails extends Component {
   };
 
   render() {
-    console.log(this.state.mediaDetails);
+    // console.log(this.state.cast)
     return (
-      <div className="container mt-5">
-        <div className="row d-flex justify-content-evenly">
-          <div className="col-8">
-            <div>
-              <h3 className="title">
-                <strong>
-                  {this.state.mediaDetails.title
-                    ? this.state.mediaDetails.title
-                    : this.state.mediaDetails.name}
-                </strong>
-              </h3>
-              {this.state.mediaDetails.tagline !== undefined ?
-              ( <span className="tagline">
-              {this.state.mediaDetails.tagline}
-              </span> ) : null}
-            </div> 
-
-            <p className="overview">{this.state.mediaDetails.overview}</p>
-
-            <div className="container">
-              <div className="row">
-                <div className="col-3">Genre</div>
-                <div className="col-3">
-                  <ul className="list-unstyled">
-                    {this.state.mediaDetails.genres !== undefined
-                      ? this.state.mediaDetails.genres.map((genre) => {
-                          return <li key={genre.id}>{genre.name}</li>;
-                        })
-                      : null}
-                  </ul>
-                </div>
+      <div>
+        <div className="container mt-5">
+          <div className="row d-flex justify-content-evenly">
+            <div className="col-8">
+              <div>
+                <h1 className="title">
+                  <strong>
+                    {this.state.mediaDetails.title
+                      ? this.state.mediaDetails.title
+                      : this.state.mediaDetails.name}
+                  </strong>
+                </h1>
+                {this.state.mediaDetails.tagline !== undefined ? (
+                  <span className="tagline">
+                    {this.state.mediaDetails.tagline}
+                  </span>
+                ) : null}
               </div>
-              {this.state.mediaDetails.runtime !== undefined ? (
-                <div className="row info">
-                  <div className="col-3">Duration</div>
+
+              <p className="overview">{this.state.mediaDetails.overview}</p>
+
+              <div className="container">
+                <div className="row">
+                  <div className="col-3">Genre</div>
                   <div className="col-3">
-                    {this.state.mediaDetails.runtime}{" "}
-                    min
+                    <ul className="list-unstyled">
+                      {this.state.mediaDetails.genres !== undefined
+                        ? this.state.mediaDetails.genres.map((genre) => {
+                            return <li key={genre.id}>{genre.name}</li>;
+                          })
+                        : null}
+                    </ul>
                   </div>
                 </div>
-              ) : null}
-              <div className="row info">
-                <div className="col-3">Movie score</div>
-                <div className="col-3">
-                  ★ {this.state.mediaDetails.vote_average}
+                {this.state.mediaDetails.runtime !== undefined ? (
+                  <div className="row info">
+                    <div className="col-3">Duration</div>
+                    <div className="col-3">
+                      {this.state.mediaDetails.runtime} min
+                    </div>
+                  </div>
+                ) : null}
+                <div className="row info">
+                  <div className="col-3">Movie score</div>
+                  <div className="col-3">
+                    ★ {this.state.mediaDetails.vote_average}
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-3">Audio languages</div>
-                <div className=" col-3">
-                  <ul className="list-unstyled">
-                    {this.state.mediaDetails.spoken_languages !== undefined
-                      ? this.state.mediaDetails.spoken_languages.map(
-                          (language) => {
-                            return <li> {language.english_name} </li>;
-                          }
-                        )
-                      : null}
-                  </ul>
+                <div className="row">
+                  <div className="col-3">Audio languages</div>
+                  <div className=" col-3">
+                    <ul className="list-unstyled">
+                      {this.state.mediaDetails.spoken_languages !== undefined
+                        ? this.state.mediaDetails.spoken_languages.map(
+                            (language) => {
+                              return <li> {language.english_name} </li>;
+                            }
+                          )
+                        : null}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="col-4">
+              <img
+                className="image"
+                src={`https://image.tmdb.org/t/p/w500${this.state.mediaDetails.poster_path}`}
+                style={{ height: "500px" }}
+                alt="poster"
+              ></img>
+            </div>
           </div>
-          <div className="col-4">
-            <img
-              className="image"
-              src={`https://image.tmdb.org/t/p/w500${this.state.mediaDetails.poster_path}`}
-              style={{ height: "600px" }}
-              alt="poster"
-            ></img>
+        </div>
+        <div className="container mt-5">
+          <h3>Casting</h3>
+          <div className="row d-flex justify-content-around">
+          {this.state.cast.filter((c) => c.profile_path !== null).slice(0, 7).map((character) => {
+              return (
+                <div className="card" style={{ width: "10rem" }}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${character.profile_path}`}
+                    className="card-img-top"
+                    alt="cast image"
+                  />
+                  <div className="card-body">
+                    <p className="card-text" style={{ color: "black" }}>
+                      <strong>{character.name}</strong> as {character.character}
+                    </p>
+                  </div>
+                </div>
+              )}
+            )}
           </div>
         </div>
       </div>
